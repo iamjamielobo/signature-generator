6e341f445bd1033e2ac9d3d6f2a1af95dffed2f5
Jamie Lobo <jamie.lobo@webklipper.com>
2022-08-10 14:55:11 +0530

    init commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a26105f83da31e50"; }
+/******/ 		__webpack_require__.h = function() { return "79354c22b9540b4e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
