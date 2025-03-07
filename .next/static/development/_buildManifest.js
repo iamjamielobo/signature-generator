06573897429615261ccf3c159c8b459ef0373544
Jamie Lobo <iamjamielobo@gmail.com>
2025-03-07 12:39:24 +0530

    new form

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/in-vino-veritos":["static\u002Fchunks\u002Fpages\u002Fin-vino-veritos.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fin-vino-veritos"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
