9117ee30c67036915fe7531cab1fe12354eb98ac
Jamie Lobo <iamjamielobo@gmail.com>
2025-03-11 11:00:44 +0530

    Final option selected

--- pages/index.js
+++ pages/index.js
@@ -101,104 +101,104 @@ class Index extends Component {
 					</html>
 			`
 
-		const htmlTemp1 = `
-					<html>
-							<body style="margin: 10px 0">
-									<table style='font-family: Helvetica;' width="652" cellspacing="0" cellpadding="0" border="0">
-											<tbody>
-													<tr>
-															<td width="140" style="vertical-align: middle; text-align: center; padding: 0 20px 0 10px">
-							<img width='110' style="border: none;" src="https://www.windsorindia.com/wp-content/uploads/2022/09/windsor-logo.png" />
-															</td>
-															<td width="1"><hr style='border:0; border-left: 2px solid black; height: 80px;' /></td>
-															<td width="380" style="vertical-align: middle; text-align: center; padding: 0 10px 0 20px;">
-																	<table cellpadding="0" border="0" style="text-align: left;" align="left">
-																			<tr>
-																					<td><span style='font-weight: bold; font-size: 18px; color: #8b69fe;'>${name}</span></td>
-																			</tr>
-																			<tr>
-																					<td style='font-size: 14px;font-weight: bold;padding: 4px 0;'>${company}</td>
-																			</tr>
-																			<tr>
-																					<td style='font-size: 14px;'>${designation}</td>
-																			</tr>
-																			<tr>
-																					<td style='padding: 4px 0;'>
-																							<table width="380" cellspacing="0" cellpadding="0" border="0">
-																									<tr>
-																											<td style='font-size: 12px;'><span style='color: #f2c201;'>P</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='tel:+${phone}'>+${phone}</a></td>
-																											<td style='font-size: 12px;'><span style='color: #f2c201;'>E</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='mailto: ${email}'>${email}</a></td>
-																									</tr>
-																							</table>
-																					</td>
-																			</tr>
-																			<tr>
-																					<td style='font-size: 12px;'><span style='color: #f2c201;'>W</span>: <a style='text-decoration: none; color: inherit;' href='https://www.windsorindia.com'>www.windsorindia.com</a></td>
-																			</tr>
-																			<tr>
-																					<td style=''><a style='color: inherit; font-size: 12px;' target='_blank' href='${linkedin}'>${linkedin}</a></td>
-																			</tr>
-																	</table>
-															</td>
-															<td width="1"><hr style='border:0; border-left: 2px solid black; height: 80px;' /></td>
-															<td width="140" style="vertical-align: middle; text-align: center; padding: 0 20px 0 40px;">
-																<img width='80' style="border: none;" src='${greatPlaceToWorkImg}' />
-															</td>
-													</tr>
-											</tbody>
-									</table>
-							</body>
-					</html>
-			`
+		// const htmlTemp1 = `
+		// 			<html>
+		// 					<body style="margin: 10px 0">
+		// 							<table style='font-family: Helvetica;' width="652" cellspacing="0" cellpadding="0" border="0">
+		// 									<tbody>
+		// 											<tr>
+		// 													<td width="140" style="vertical-align: middle; text-align: center; padding: 0 20px 0 10px">
+		// 					<img width='110' style="border: none;" src="https://www.windsorindia.com/wp-content/uploads/2022/09/windsor-logo.png" />
+		// 													</td>
+		// 													<td width="1"><hr style='border:0; border-left: 2px solid black; height: 80px;' /></td>
+		// 													<td width="380" style="vertical-align: middle; text-align: center; padding: 0 10px 0 20px;">
+		// 															<table cellpadding="0" border="0" style="text-align: left;" align="left">
+		// 																	<tr>
+		// 																			<td><span style='font-weight: bold; font-size: 18px; color: #8b69fe;'>${name}</span></td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='font-size: 14px;font-weight: bold;padding: 4px 0;'>${company}</td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='font-size: 14px;'>${designation}</td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='padding: 4px 0;'>
+		// 																					<table width="380" cellspacing="0" cellpadding="0" border="0">
+		// 																							<tr>
+		// 																									<td style='font-size: 12px;'><span style='color: #f2c201;'>P</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='tel:+${phone}'>+${phone}</a></td>
+		// 																									<td style='font-size: 12px;'><span style='color: #f2c201;'>E</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='mailto: ${email}'>${email}</a></td>
+		// 																							</tr>
+		// 																					</table>
+		// 																			</td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='font-size: 12px;'><span style='color: #f2c201;'>W</span>: <a style='text-decoration: none; color: inherit;' href='https://www.windsorindia.com'>www.windsorindia.com</a></td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style=''><a style='color: inherit; font-size: 12px;' target='_blank' href='${linkedin}'>${linkedin}</a></td>
+		// 																	</tr>
+		// 															</table>
+		// 													</td>
+		// 													<td width="1"><hr style='border:0; border-left: 2px solid black; height: 80px;' /></td>
+		// 													<td width="140" style="vertical-align: middle; text-align: center; padding: 0 20px 0 40px;">
+		// 														<img width='80' style="border: none;" src='${greatPlaceToWorkImg}' />
+		// 													</td>
+		// 											</tr>
+		// 									</tbody>
+		// 							</table>
+		// 					</body>
+		// 			</html>
+		// 	`
 
-		const htmlTemp2 = `
-					<html>
-							<body style="margin: 10px 0">
-									<table style='font-family: Helvetica;border:1px solid #dbdbdb;border-radius: 5px;' width="700" cellspacing="0" cellpadding="0" border="0">
-											<tbody>
-													<tr>
-															<td width="380" style="vertical-align: middle; text-align: center; padding: 15px 0px 15px 10px;">
-																	<table cellpadding="0" border="0" style="text-align: left;" align="left">
-																			<tr>
-																					<td><span style='font-weight: bold; font-size: 18px; color: #8b69fe;'>${name}</span></td>
-																			</tr>
-																			<tr>
-																					<td style='font-size: 14px;font-weight: bold;padding: 4px 0;'>${company}</td>
-																			</tr>
-																			<tr>
-																					<td style='font-size: 14px;padding-bottom: 0px;'>${designation}</td>
-																			</tr>
-																			<tr>
-																					<td style='padding: 4px 0;'>
-																							<table width="380" cellspacing="0" cellpadding="0" border="0">
-																									<tr>
-																											<td style='font-size: 12px;'><span style='color: #f2c201;'>P</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='tel:+${phone}'>+${phone}</a></td>
-																											<td style='font-size: 12px;'><span style='color: #f2c201;'>E</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='mailto: ${email}'>${email}</a></td>
-																									</tr>
-																							</table>
-																					</td>
-																			</tr>
-																			<tr>
-																					<td style='font-size: 12px;'><span style='color: #f2c201;'>W</span>: <a style='text-decoration: none; color: inherit;' href='https://www.windsorindia.com'>www.windsorindia.com</a></td>
-																			</tr>
-																			<tr>
-																					<td style=''><a style='color: inherit; font-size: 12px;' target='_blank' href='${linkedin}'>${linkedin}</a></td>
-																			</tr>
-																	</table>
-															</td>
-															<td width="1" style=''><hr style='border:0; border-left: 2px solid #dbdbdb; height: 120px;' /></td>
-															<td width="130" style="vertical-align: middle; text-align: center; padding: 0 0px 0 20px;">
-																<img width='110' style="border: none;" src="https://www.windsorindia.com/wp-content/uploads/2022/09/windsor-logo.png" />
-															</td>
-															<td width="120" style="vertical-align: baseline; text-align: center; padding: 0 10px">
-																	<img width='100' style="border: none;" src='${greatPlaceToWorkImg}' />
-															</td>
-													</tr>
-											</tbody>
-									</table>
-							</body>
-					</html>
-			`
+		// const htmlTemp2 = `
+		// 			<html>
+		// 					<body style="margin: 10px 0">
+		// 							<table style='font-family: Helvetica;border:1px solid #dbdbdb;border-radius: 5px;' width="700" cellspacing="0" cellpadding="0" border="0">
+		// 									<tbody>
+		// 											<tr>
+		// 													<td width="380" style="vertical-align: middle; text-align: center; padding: 15px 0px 15px 10px;">
+		// 															<table cellpadding="0" border="0" style="text-align: left;" align="left">
+		// 																	<tr>
+		// 																			<td><span style='font-weight: bold; font-size: 18px; color: #8b69fe;'>${name}</span></td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='font-size: 14px;font-weight: bold;padding: 4px 0;'>${company}</td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='font-size: 14px;padding-bottom: 0px;'>${designation}</td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='padding: 4px 0;'>
+		// 																					<table width="380" cellspacing="0" cellpadding="0" border="0">
+		// 																							<tr>
+		// 																									<td style='font-size: 12px;'><span style='color: #f2c201;'>P</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='tel:+${phone}'>+${phone}</a></td>
+		// 																									<td style='font-size: 12px;'><span style='color: #f2c201;'>E</span>: <a  target='_blank' style='text-decoration: none; color: inherit;' href='mailto: ${email}'>${email}</a></td>
+		// 																							</tr>
+		// 																					</table>
+		// 																			</td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style='font-size: 12px;'><span style='color: #f2c201;'>W</span>: <a style='text-decoration: none; color: inherit;' href='https://www.windsorindia.com'>www.windsorindia.com</a></td>
+		// 																	</tr>
+		// 																	<tr>
+		// 																			<td style=''><a style='color: inherit; font-size: 12px;' target='_blank' href='${linkedin}'>${linkedin}</a></td>
+		// 																	</tr>
+		// 															</table>
+		// 													</td>
+		// 													<td width="1" style=''><hr style='border:0; border-left: 2px solid #dbdbdb; height: 120px;' /></td>
+		// 													<td width="130" style="vertical-align: middle; text-align: center; padding: 0 0px 0 20px;">
+		// 														<img width='110' style="border: none;" src="https://www.windsorindia.com/wp-content/uploads/2022/09/windsor-logo.png" />
+		// 													</td>
+		// 													<td width="120" style="vertical-align: baseline; text-align: center; padding: 0 10px">
+		// 															<img width='100' style="border: none;" src='${greatPlaceToWorkImg}' />
+		// 													</td>
+		// 											</tr>
+		// 									</tbody>
+		// 							</table>
+		// 					</body>
+		// 			</html>
+		// 	`
 
 		return (
 			<div style={{ padding: '0 50px' }}>
@@ -252,7 +252,6 @@ class Index extends Component {
 					</div>
 				</div>
 				<div style={{ marginBottom: '50px' }}>
-					<p style={{ fontSize: '21px', fontWeight: 'bold' }}>Option 1</p>
 					<div
 						width='100%'
 						height='150'
@@ -261,14 +260,14 @@ class Index extends Component {
 					<h1 style={{ display: 'flex', alignItems: 'center' }}>
 						<button
 							onClick={() => myFunction(html)}>
-							Copy Option 1 HTML
+							HTML
 						</button>
 					</h1>
 					<div style={{ marginBottom: '50px' }}>
 						{html}
 					</div>
 				</div>
-				<div style={{ marginBottom: '50px' }}>
+				{/* <div style={{ marginBottom: '50px' }}>
 
 					<p style={{ fontSize: '21px', fontWeight: 'bold' }}>Option 2</p>
 
@@ -302,7 +301,7 @@ class Index extends Component {
 					<div style={{ marginBottom: '50px' }}>
 						{htmlTemp2}
 					</div>
-				</div>
+				</div> */}
 			</div>
 		)
 	}
